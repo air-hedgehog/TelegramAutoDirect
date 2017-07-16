41f660885456ff67a76447f0fc69e5c382115669
antony <metalization25@gmail.com>
2017-07-16 18:32:15 +0300

    UI

--- app/src/main/java/com/akimchenko/antony/messengerautoreply/MainActivity.java
+++ app/src/main/java/com/akimchenko/antony/messengerautoreply/MainActivity.java
@@ -1,6 +1,7 @@
 package com.akimchenko.antony.messengerautoreply;
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +12,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String RESPONSE_STRING_SP_KEY = "responseStringKey";
+    private static final String SKYPE_LOGIN_SP_KEY = "SkypeLoginKey";
+    private static final String VIBER_LOGIN_SP_KEY = "ViberLoginKey";
+    private static final String TELEGRAM_ATTACHED_SP_KEY = "TelegramAttachedKey";
+    private static final String TELEGRAM_LINK_SP_KEY = "TelegramLinkKey";
+
+    private SharedPreferences sharedPreferences;
+    private MainFragment mainFragment;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
         setContentView(R.layout.activity_main);
-        pushFragment(new MainFragment());
+        mainFragment = new MainFragment();
+        pushFragment(mainFragment);
     }
 
     public void pushFragment(Fragment fragment) {
@@ -25,5 +37,48 @@ public class MainActivity extends AppCompatActivity {
                     .addToBackStack(fragment.getClass().getName())
                     .commitAllowingStateLoss();
         }
+    }
+
+    public void setSkypeLoggedIn(boolean isLoggedIn) {
+        sharedPreferences.edit().putBoolean(SKYPE_LOGIN_SP_KEY, isLoggedIn).apply();
+    }
+
+    public void setViberLogedIn(boolean isLoggedIn) {
+        sharedPreferences.edit().putBoolean(VIBER_LOGIN_SP_KEY, isLoggedIn).apply();
+    }
+
+    public void setTelegramAttached(boolean isAttached) {
+        sharedPreferences.edit().putBoolean(TELEGRAM_ATTACHED_SP_KEY, isAttached).apply();
+    }
+
+    public boolean isSkypeLoggedIn() {
+        return sharedPreferences.getBoolean(SKYPE_LOGIN_SP_KEY, false);
+    }
+
+    public boolean isViberLoggedIn() {
+        return sharedPreferences.getBoolean(VIBER_LOGIN_SP_KEY, false);
+    }
+
+    public boolean isTelegramAttached() {
+        return sharedPreferences.getBoolean(TELEGRAM_ATTACHED_SP_KEY, false);
+    }
+
+    public void setTelegramLink(String link) {
+        sharedPreferences.edit().putString(TELEGRAM_LINK_SP_KEY, link).apply();
+    }
+
+    public String getTelegramLink() {
+        return sharedPreferences.getString(TELEGRAM_LINK_SP_KEY, "");
+    }
+
+    public void setResponseString(String string) {
+        if (!isTelegramAttached() && string.endsWith(getTelegramLink())) {
+            string.replaceAll(getTelegramLink(), "");
+        }
+        sharedPreferences.edit().putString(RESPONSE_STRING_SP_KEY, string).apply();
+    }
+
+    public String getResponseString() {
+        return sharedPreferences.getString(RESPONSE_STRING_SP_KEY, "");
     }
 }
--- app/src/main/java/com/akimchenko/antony/messengerautoreply/MainFragment.java
+++ app/src/main/java/com/akimchenko/antony/messengerautoreply/MainFragment.java
@@ -3,10 +3,16 @@ package com.akimchenko.antony.messengerautoreply;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by antony on 7/15/17.
@@ -20,9 +26,39 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, null);
-        EditText autoMessage = (EditText) view.findViewById(R.id.auto_message_edit_text);
-        savedInstanceState.putString(MESSAGE_BUNDLE_KEY, autoMessage.getText().toString());
-        autoMessage.setText(savedInstanceState.getString(MESSAGE_BUNDLE_KEY));
+        MainActivity activity = (MainActivity) getActivity();
+        if (activity == null) return view;
+        EditText autoMessage = view.findViewById(R.id.auto_message_edit_text);
+        ImageView skypeButton = view.findViewById(R.id.skype_button);
+        ImageView viberbutton = view.findViewById(R.id.viber_button);
+        RelativeLayout telegramButton = view.findViewById(R.id.telegram_link_add);
+        ImageView telegramIcon = telegramButton.findViewById(R.id.telegram_button_icon);
+        TextView telegramButtonText = view.findViewById(R.id.telegram_button_text);
+        skypeButton.setImageDrawable(ContextCompat.getDrawable(activity, activity.isSkypeLoggedIn() ? R.drawable.ic_skype : R.drawable.ic_skype_disabled));
+        viberbutton.setImageDrawable(ContextCompat.getDrawable(activity, activity.isViberLoggedIn() ? R.drawable.ic_viber : R.drawable.ic_viber_disabled));
+        telegramIcon.setImageDrawable(ContextCompat.getDrawable(activity, activity.isTelegramAttached() ? R.drawable.ic_telegram : R.drawable.ic_telegram_disabled));
+        telegramButtonText.setText(activity.getResources().getString(activity.isTelegramAttached() ? R.string.telegram_link_attached : R.string.telegram_link_not_attached));
+        telegramButtonText.setTextColor(ContextCompat.getColor(activity, activity.isTelegramAttached() ? R.color.colorAccent : R.color.greyInactive));
+
+        autoMessage.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity activity1 = (MainActivity) getActivity();
+                if (activity1 == null) return;
+                activity1.setResponseString(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        autoMessage.setText(activity.getResponseString());
         return view;
     }
 }
